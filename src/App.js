1f5cb2b8d4be377f9541e49b5f08e50c78136595
nizihad824 <mdnurulislam97@gmail.com>
2023-03-21 12:47:55 +0100

    navbar from daisy ui customized

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import router from './Routes/Routes';
 
 function App() {
   return (
-    <div>
+    <div className='max-w-[1440px] mx-auto'>
       <RouterProvider router={router} />
     </div>
   );
